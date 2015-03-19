a05ba0de3779cacd74840c56e60777696f404708
Igor Cactusson <stalcone@yahoo.com>
2015-03-19 17:52:46 +0300

    first commit

---
+++ oblivion/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from . import views
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.HomePageView.as_view(), name='home'),
+)
